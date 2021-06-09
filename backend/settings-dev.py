1acba40acf39d162e81841b18e71454f7fb557d4
Greattony Oduche <greattony96@gmail.com>
2021-06-10 02:32:49 +0300

    Postgres Setup

--- backend/settings-dev.py
+++ backend/settings-dev.py
@@ -37,11 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'tip_api',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tipstream',
+        'USER': 'tipstream',
+        'PASSWORD': 'tipstream',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
@@ -123,3 +131,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
---
+++ tip_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TipApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tip_api'
